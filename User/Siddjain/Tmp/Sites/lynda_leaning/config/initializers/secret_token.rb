b4ef1adbe92bff370b7006156cdc9ffef674e884
Siddharth Jain <s8002.sid@gmail.com>
2014-04-26 16:00:14 +0530

    Just started Learning Ruby

---
+++ User/Siddjain/Tmp/Sites/lynda_leaning/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LyndaLeaning::Application.config.secret_key_base = 'd5e6ecda1340083560b2a163ebbeb989341c192fb071bc6168512762bc9d0a9db68f887658af9f3a98e298e08ccd4e8c2e8ed7478d54933ebaf6d38f3f78ea75'
